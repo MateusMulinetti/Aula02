43dc2aa47143dd754e00c29685ac63db74e2065f
Mateus <mmulinete010@gmail.com>
2025-02-25 21:03:55 -0300

    portuguese

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
         hello();
+        ola();
     }
     public static void hello() {
         System.out.println("Hello world");
+    }
+    public static void ola() {
+        System.out.println("ola mundo");
     }
 }
